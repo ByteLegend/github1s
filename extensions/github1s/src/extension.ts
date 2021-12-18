b33dd18b1619d2e1869a5d84a35aed011ad8b715
Bo Zhang <bo@bytelegend.com>
2021-12-18 15:52:08 +0800

    Hacky way to fix open preview issue with initReadme

--- extensions/github1s/src/extension.ts
+++ extensions/github1s/src/extension.ts
@@ -4,19 +4,20 @@
  */
 
 import * as vscode from 'vscode';
-import { commands, TreeItemCollapsibleState, Uri } from 'vscode';
-import { getExtensionContext, setExtensionContext } from '@/helpers/context';
-import { registerGitHub1sCommands } from '@/commands';
-import { registerVSCodeProviders } from '@/providers';
-import { GitHub1sFileSystemProvider } from '@/providers/fileSystemProvider';
+import {commands, TreeItemCollapsibleState, Uri} from 'vscode';
+import {getExtensionContext, setExtensionContext} from '@/helpers/context';
+import {registerGitHub1sCommands} from '@/commands';
+import {registerVSCodeProviders} from '@/providers';
+import {GitHub1sFileSystemProvider} from '@/providers/fileSystemProvider';
 import router from '@/router';
-import { activateSourceControl } from '@/source-control';
-import { registerEventListeners } from '@/listeners';
-import { PageType } from './router/types';
-import { byteLegendContext } from '@/bytelegend/bytelegendContext';
-import { open, registerByteLegendCommands } from '@/bytelegend/commands';
-import { MyAnswerTreeDataProvider } from '@/bytelegend/my-answer-list-view';
-import { TutorialsView } from '@/bytelegend/tutorials-view';
+import {activateSourceControl} from '@/source-control';
+import {registerEventListeners} from '@/listeners';
+import {PageType} from './router/types';
+import {byteLegendContext} from '@/bytelegend/bytelegendContext';
+import {open, registerByteLegendCommands} from '@/bytelegend/commands';
+import {MyAnswerTreeDataProvider} from '@/bytelegend/my-answer-list-view';
+import {TutorialsView} from '@/bytelegend/tutorials-view';
+import {sleep} from "@/bytelegend/utils";
 
 export async function activate(context: vscode.ExtensionContext) {
 	const browserUrl = (await vscode.commands.executeCommand(
@@ -59,14 +60,9 @@ export async function activate(context: vscode.ExtensionContext) {
 		}
 	});
 
-	const activityBarVisibleNow = await vscode.commands.executeCommand(
-		'bytelegend.isActivityBarVisible'
-	);
+	const activityBarVisibleNow = await vscode.commands.executeCommand('bytelegend.isActivityBarVisible');
 	const expectedActivityBarVisible = byteLegendContext.showActivityBar;
-	if (
-		(activityBarVisibleNow && !expectedActivityBarVisible) ||
-		(!activityBarVisibleNow && expectedActivityBarVisible)
-	) {
+	if ((activityBarVisibleNow && !expectedActivityBarVisible) || (!activityBarVisibleNow && expectedActivityBarVisible)) {
 		await vscode.commands.executeCommand(
 			'workbench.action.toggleActivityBarVisibility'
 		);
@@ -82,9 +78,7 @@ export async function activate(context: vscode.ExtensionContext) {
 	if (byteLegendContext.initReadme) {
 		await vscode.commands.executeCommand('workbench.action.closeAllEditors');
 		if (byteLegendContext.initReadme.startsWith('https://')) {
-			await open(
-				byteLegendContext.initReadme.replace('https://', 'github1s://')
-			);
+			await open(byteLegendContext.initReadme.replace('https://', 'github1s://'));
 		} else {
 			await open(byteLegendContext.initReadme);
 		}
@@ -103,7 +97,7 @@ async function registerByteLegendViews() {
 	myAnswerTreeView.onDidChangeSelection((e) => {
 		const oldState = e.selection[0].collapsibleState;
 		if (oldState == TreeItemCollapsibleState.Collapsed) {
-			myAnswerTreeView.reveal(e.selection[0], { expand: true });
+			myAnswerTreeView.reveal(e.selection[0], {expand: true});
 		}
 	});
 
@@ -117,30 +111,44 @@ async function registerByteLegendViews() {
 }
 
 function getFileName(filePath: string) {
-	const lastIndexOfSlash = filePath.indexOf('/');
+	const lastIndexOfSlash = filePath.lastIndexOf('/');
 	return filePath.substring(lastIndexOfSlash + 1);
 }
 
+/*
+This is a bit tricky. Sometimes, things may happen in the following order:
+
+- Open whatever.md from previous history.
+- Triggers `onDidOpenTextDocument`.
+- We fire 'workbench.action.closeAllEditors' to close all editors.
+- We open initReadme
+- The preview of whatever.md pops up as the active tab, hiding initReadme.
+
+In this method, when we want to open preview, we check if the markdown file is a visible tab,
+and only preview if it's visible. If it's not visible, poll for at most 5 seconds.
+ */
 async function openMarkdownPreview(initBrowserUrl: string, uri: Uri) {
-	const tabGroups: string[][] = await vscode.commands.executeCommand(
-		'bytelegend.getEditorTabGroups'
-	);
-	const flatTabs = Array.prototype.concat.apply([], tabGroups);
-	const fileName = getFileName(uri.path);
-	if (!flatTabs.find((tab) => tab == fileName)) {
-		// we may have already fired `closeAllEditors` command, don't show the preview for the closed markdown tab
-		return;
+	const deadline = new Date().getTime() + 5000
+	while (new Date().getTime() < deadline) {
+		const tabGroups: string[][] = await vscode.commands.executeCommand('bytelegend.getEditorTabGroups');
+		const flatTabs = Array.prototype.concat.apply([], tabGroups);
+		const fileName = getFileName(uri.path);
+		console.log(`tabs: ${JSON.stringify(flatTabs)}, file: ${fileName}`)
+		if (flatTabs.find((tab) => tab == fileName)) {
+			// we may have already fired `closeAllEditors` command, don't show the preview for the closed markdown tab
+			await doOpenPreview(initBrowserUrl, uri, tabGroups)
+			return
+		}
+		await sleep(500)
 	}
 
-	await vscode.commands.executeCommand('markdown.showPreview', null, [uri], {
-		locked: true,
-	});
+	console.warn(`Fail to get open tab matching ${uri.toString()}, skip opening preview.`)
+}
 
-	if (
-		uri.toString().endsWith('README.md') &&
-		initBrowserUrl.endsWith('README.md') &&
-		tabGroups.length == 1
-	) {
+async function doOpenPreview(initBrowserUrl: string, uri: Uri, tabGroups: string[][]) {
+	await vscode.commands.executeCommand('markdown.showPreview', null, [uri], {locked: true});
+
+	if (uri.toString().endsWith('README.md') && initBrowserUrl.endsWith('README.md') && tabGroups.length == 1) {
 		// only open the challenged file upon first open
 		const initData = await commands.executeCommand('bytelegend.getInitData');
 		const whitelist = initData?.['whitelist'] || [];
@@ -161,27 +169,27 @@ async function openMarkdownPreview(initBrowserUrl: string, uri: Uri) {
 // initialize the VSCode's state according to the router url
 export const initialVSCodeState = async () => {
 	const routerState = await router.getState();
-	const { filePath, pageType } = routerState;
+	const {filePath, pageType} = routerState;
 	const scheme = GitHub1sFileSystemProvider.scheme;
 
 	if (filePath && pageType === PageType.TREE) {
 		vscode.commands.executeCommand(
 			'revealInExplorer',
-			vscode.Uri.parse('').with({ scheme, path: filePath })
+			vscode.Uri.parse('').with({scheme, path: filePath})
 		);
 	} else if (filePath && pageType === PageType.BLOB) {
-		const { startLineNumber, endLineNumber } = routerState;
+		const {startLineNumber, endLineNumber} = routerState;
 		const start = new vscode.Position(startLineNumber - 1, 0);
 		const end = new vscode.Position(endLineNumber - 1, 999999);
 		const documentShowOptions: vscode.TextDocumentShowOptions = startLineNumber
-			? { selection: new vscode.Range(start, end) }
+			? {selection: new vscode.Range(start, end)}
 			: {};
 
 		// TODO: the selection of the opening file may be cleared
 		// when editor try to restore previous state in the same file
 		vscode.commands.executeCommand(
 			'vscode.open',
-			vscode.Uri.parse('').with({ scheme, path: filePath }),
+			vscode.Uri.parse('').with({scheme, path: filePath}),
 			documentShowOptions
 		);
 	} else if (pageType === PageType.PULL_LIST) {
